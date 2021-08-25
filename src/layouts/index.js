7571630ca1dacf3584813a169281b3e0e3489a17
Stackbit Code Editor <projects@stackbit.com>
2021-08-25 21:49:40 +0000

    index.js: changed

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,7 +2,6 @@ import advanced from './advanced';
 import category from './category';
 import home from './home';
 import product from './product';
-#import store from './store';
 
 export {
     advanced,
