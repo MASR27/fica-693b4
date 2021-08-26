5915a5e1b648834bab0f99a6f00e4bd0b27d87e7
Stackbit Code Editor <projects@stackbit.com>
2021-08-26 01:08:09 +0000

    category.js: changed

--- src/layouts/category.js
+++ src/layouts/category.js
@@ -3,14 +3,16 @@
     <input type="text" name="name">
     <input type="email" name="email">
     <input type="text" name="message">
-    
+
     <!-- checkbox handle --> 
     <input type="checkbox" name="subscribe" value="yes" checked>
     <input type="hidden" name="subscribe" value="no">
+    
     <!-- radio button handle --> 
     <input type="radio" name="gender" value="male" checked>
     <input type="radio" name="gender" value="female">
     <input type="radio" name="gender" value="other">
+    
     <!-- select field handle --> 
     <select name="work-experience">
         <option value="one-year">0-1 years</option>
