c6618c1510a5938119c44be22a753a3ebbc81f4b
Stackbit Code Editor <projects@stackbit.com>
2021-08-26 20:23:23 +0000

    ContactSection.js: changed

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -8,7 +8,7 @@ export default class ContactSection extends React.Component {
         let section = _.get(this.props, 'section', null);
         return (
             <section className="content__row content__row--full-width contact__section" data-id={_.get(section, 'section_id', null)}>
-              
+              <script id="mcjs">!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/f7a241f98ff6af92f82778ec5/843f6c80d53aee7407dc4ca60.js");</script>
             </section>
         );
     }
