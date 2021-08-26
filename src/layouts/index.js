b8b90d7fb66569bedccfd7908c0a0c3a6d4ffaf8
Stackbit Code Editor <projects@stackbit.com>
2021-08-26 21:24:45 +0000

    index.js: changed

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -5,7 +5,7 @@ import product from './product';
 import store from './store';
 import initNetlifyIdentity
 
-initNetlifyIdentity() {
+export{function initNetlifyIdentity() {
     console.log("initNetlifyIdentity called.");
     const script = document.createElement("script");
 
