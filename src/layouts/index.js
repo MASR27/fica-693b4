6b5563c8538b3e3420a092334163ab3b665f02ae
Stackbit Code Editor <projects@stackbit.com>
2021-08-26 22:09:15 +0000

    index.js: changed

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,39 +4,40 @@ import home from './home';
 import product from './product';
 import store from './store';
 
-function initNetlifyIdentity() {
-    console.log("initNetlifyIdentity called.");
-    const script = document.createElement("script");
+import React, { Component } from 'react'
 
-    script.src = "https://us5.list-manage.com/contact-form?u=f7a241f98ff6af92f82778ec5&form_id=0b5570499c8ed7ccdfdda4ff5d76c90c"
-    script.async = true;
+import { NetlifyForm, Honeypot } from 'react-netlify-forms'
 
-    document.dody.appendChild(script);
-
-<form name="simpleContactForm" method="POST" data-netlify="true" id="simple-contact-form" class="contact-form">    <p class="form-row">
-        <label id="contact-form-name-label" for="contact-form-name" class="form-label">Name</label>
-        <input type="text" name="name" id="contact-form-name" aria-labelledby="contact-form-name-label" class="form-input" />
-    </p>
-    <p class="form-row">
-        <label id="contact-form-email-label" for="contact-form-email" class="form-label">Email address</label>
-        <input type="email" name="email" id="contact-form-email" aria-labelledby="contact-form-email-label" class="form-input" />
-    </p>
-    <p class="form-row">
-        <label id="contact-form-message-label" for="contact-form-message" class="form-label">Message</label>
-        <textarea
-            name="message"
-            id="contact-form-message"
-            aria-labelledby="contact-form-message-label"
-            class="form-textarea"
-            rows="7"
-        ></textarea>
-    </p>
-    <p class="form-row form-submit">
-        <button type="submit" class="button">Send Message</button>
-    </p>
-</form>
-
-    }
+export default ContactForm = () => (
+  <NetlifyForm name='Contact' action='/thanks' honeypotName='bot-field'>
+    {({ handleChange, success, error }) => (
+      <>
+        <Honeypot />
+        {success && <p>Thanks for contacting us!</p>}
+        {error && (
+          <p>Sorry, we could not reach our servers. Please try again later.</p>
+        )}
+        <div>
+          <label htmlFor='name'>Name:</label>
+          <input type='text' name='name' id='name' onChange={handleChange} />
+        </div>
+        <div>
+          <label htmlFor='message'>Message:</label>
+          <textarea
+            type='text'
+            name='message'
+            id='message'
+            rows='4'
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <button type='submit'>Submit</button>
+        </div>
+      </>
+    )}
+  </NetlifyForm>
+)
 
 export {
     advanced,
