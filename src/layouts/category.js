780c782652023c436433e17e0b10a55e2521ed9a
Stackbit Code Editor <projects@stackbit.com>
2021-08-26 01:07:43 +0000

    category.js: changed

--- src/layouts/category.js
+++ src/layouts/category.js
@@ -1,36 +1,19 @@
-import React from 'react';
-import _ from 'lodash';
-
-import components, {Layout} from '../components/index';
-import {getPage} from '../utils';
-
-export default class Category extends React.Component {
-    render() {
-        let store_page = getPage(this.props.pages, '/store');
-        return (
-            <Layout {...this.props}>
-            <main className={'content' + (_.get(store_page, 'frontmatter.page_css_class', null) ? (' ' + _.get(store_page, 'frontmatter.page_css_class', null)) : '')}>
-                {_.map(_.get(store_page, 'frontmatter.sections', null), (section, section_idx) => (
-                    (_.get(section, 'type', null) === 'store_section') ? ((() => {
-                        let component = _.upperFirst(_.camelCase(_.get(section, 'type', null)));
-                        let Component = components[component];
-                        return (
-                            <Component key={section_idx} {...this.props} section={section} page={this.props.page} category_url={_.get(this.props, 'page.__metadata.urlPath', null)} site={this.props} />
-                        );
-                    })()) : (() => {
-                        let component = _.upperFirst(_.camelCase(_.get(section, 'type', null)));
-                        let Component = components[component];
-                        return ((() => {
-                            let component = _.upperFirst(_.camelCase(_.get(section, 'type', null)));
-                            let Component = components[component];
-                            return (
-                                <Component key={section_idx} {...this.props} section={section} page={this.props.page} category_url={_.get(this.props, 'page.__metadata.urlPath', null)} site={this.props} />
-                            );
-                        })());
-                    })()
-                ))}
-            </main>
-            </Layout>
-        );
-    }
-}
+<form action="https://getform.io/f/4f5908b4-11ba-4fe1-96e1-2bc10a9864ee" method="POST">
+    <input type="text" name="name">
+    <input type="email" name="email">
+    <input type="text" name="message">
+    <!-- checkbox handle --> 
+    <input type="checkbox" name="subscribe" value="yes" checked>
+    <input type="hidden" name="subscribe" value="no">
+    <!-- radio button handle --> 
+    <input type="radio" name="gender" value="male" checked>
+    <input type="radio" name="gender" value="female">
+    <input type="radio" name="gender" value="other">
+    <!-- select field handle --> 
+    <select name="work-experience">
+        <option value="one-year">0-1 years</option>
+        <option value="one-five-years">1-5 years</option>
+        <option value="five-plus-years">5+ years</option>
+    </select>
+    <button type="submit">Send</button>
+</form>
