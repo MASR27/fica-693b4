d7212efec6b56bfc6af63799ecf63bd540450d1b
Stackbit Code Editor <projects@stackbit.com>
2021-08-26 01:08:04 +0000

    category.js: changed

--- src/layouts/category.js
+++ src/layouts/category.js
@@ -1,7 +1,9 @@
+
 <form action="https://getform.io/f/4f5908b4-11ba-4fe1-96e1-2bc10a9864ee" method="POST">
     <input type="text" name="name">
     <input type="email" name="email">
     <input type="text" name="message">
+    
     <!-- checkbox handle --> 
     <input type="checkbox" name="subscribe" value="yes" checked>
     <input type="hidden" name="subscribe" value="no">
