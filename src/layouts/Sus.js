61e292dc787c8e2bb4aef9c069eadc39591ea09e
Stackbit Code Editor <projects@stackbit.com>
2021-08-26 01:15:40 +0000

    Sus.js: changed

--- src/layouts/Sus.js
+++ src/layouts/Sus.js
@@ -23,3 +23,9 @@ layout: advanced
 </form>
 
 
+<form action="https://getform.io/f/{your-form-endpoint}" method="POST">
+  <input type="text" name="name">
+  <input type="email" name="email">
+  <input type="text" name="message">
+  <button type="submit">Send</button>
+</form>
