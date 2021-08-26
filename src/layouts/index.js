b9ac6999ee4b58001974e49d40a91c0000a6c4aa
Stackbit Code Editor <projects@stackbit.com>
2021-08-26 21:14:45 +0000

    index.js: changed

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -15,11 +15,6 @@ function initNetlifyIdentify() {
 }
 
 <link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
-<style type="text/css">
-	#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
-	/* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
-	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
-</style>
 <div id="mc_embed_signup">
 <form action="https://gmail.us5.list-manage.com/subscribe/post?u=f7a241f98ff6af92f82778ec5&amp;id=67b5922ad7" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
     <div id="mc_embed_signup_scroll">
